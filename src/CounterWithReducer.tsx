7e0e0f191dd105c4c7d7f4def8985971ec988df4
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-01-19 00:44:31 +0900

    useReducer基礎

--- src/CounterWithReducer.tsx
+++ src/CounterWithReducer.tsx
@@ -1,20 +1,31 @@
 import React, { useReducer } from 'react';
 
-const initialState = { count: 0 };
+type StateType = { count: number };
+const initialState: StateType = { count: 0 };
+type ActionType = { type: 'increment' | 'decrement' | 'reset' };
 
-function reducer(state: any, action: any) {
+function reducer(state: StateType, action: ActionType): StateType | never {
   switch (action.type) {
     case 'increment':
       return { count: state.count + 1 };
     case 'decrement':
       return { count: state.count - 1 };
+    case 'reset':
+      return initialState;
     default:
       throw new Error();
   }
 }
 function CounterWithReducer() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  return <div></div>;
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch({ type: 'increment' })}>+1</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-1</button>
+      <button onClick={() => dispatch({ type: 'reset' })}>reset</button>
+    </>
+  );
 }
 
 export default CounterWithReducer;
